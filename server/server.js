8b5d8db02583584f1b9f8046a13682b2dbf76d76
Gene83 <gene83@protonmail.com>
2019-02-20 13:54:11 -1000

    move static above bodyParser

--- server/server.js
+++ server/server.js
@@ -7,12 +7,11 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+app.use(express.static('../public'));
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
 app.use('/api', api);
-
-app.use(express.static('../public'));
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
