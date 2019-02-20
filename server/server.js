01058edfbbd1d3a272a6c0db3d541e9790cdd925
Gene83 <gene83@protonmail.com>
2019-02-19 19:43:17 -1000

    set up api router for routes

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const api = require('./routes/api');
+
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
+
+app.use('/api', api);
 
 app.use(express.static('../public'));
 
