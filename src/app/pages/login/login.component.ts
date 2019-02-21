ee5df15399aaa45943a1a50db2b8a6463d9c8c5b
Gene83 <gene83@protonmail.com>
2019-02-20 19:43:21 -1000

    add form validation for login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -17,4 +17,36 @@ export class LoginComponent {
   isPasswordInvalid: boolean = true;
 
   constructor() {}
+
+  validateUsername() {
+    const { username } = this.formData;
+
+    if (!username) {
+      this.isUsernameInvalid = true;
+    } else if (username.length < 3) {
+      this.isUsernameInvalid = true;
+    } else if (username.length > 16) {
+      this.isUsernameInvalid = true;
+    } else if (username.match(/\W/)) {
+      this.isUsernameInvalid = true;
+    } else {
+      this.isUsernameInvalid = false;
+    }
+  }
+
+  validatepassword() {
+    const { password } = this.formData;
+
+    if (!password) {
+      this.isPasswordInvalid = true;
+    } else if (password.length < 3) {
+      this.isPasswordInvalid = true;
+    } else if (password.length > 16) {
+      this.isPasswordInvalid = true;
+    } else if (password.match(/\W/)) {
+      this.isPasswordInvalid = true;
+    } else {
+      this.isPasswordInvalid = false;
+    }
+  }
 }
