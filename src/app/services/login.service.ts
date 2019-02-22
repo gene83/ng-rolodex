4ead2a236b9558707c65d506bb611e04d1c9d937
Gene83 <gene83@protonmail.com>
2019-02-21 16:27:20 -1000

    set up proxy for angular to localhost

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -8,16 +8,6 @@ export class LoginService {
   constructor(private http: HttpClient) {}
 
   login(data) {
-    const headerDict = {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-      'Access-Control-Allow-Headers': 'Content-Type'
-    };
-
-    const reqOptions = {
-      headers: new HttpHeaders(headerDict)
-    };
-
-    return this.http.post('http://localhost:8080/api/login', data).toPromise();
+    return this.http.post('/api/login', data).toPromise();
   }
 }
