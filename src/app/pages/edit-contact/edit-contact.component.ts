f66d5216755309b64a06fb908e2e21296b645a27
Gene83 <gene83@protonmail.com>
2019-02-22 23:37:13 -1000

    make files for edit-contact component and write out component.ts

---
+++ src/app/pages/edit-contact/edit-contact.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { BackendService } from '../../services/backend.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  templateUrl: './edit-contact.component.html',
+  styleUrls: ['./edit-contact.component.html']
+})
+export class EditContactComponent {
+  contactId: number;
+  contact: Object;
+  editedContact: Object;
+  error: string = '';
+
+  constructor(
+    private backend: BackendService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.contactId = +this.route.snapshot.paramMap.get('id');
+
+    this.backend
+      .getContact(this.contactId)
+      .then(contact => {
+        if (contact) {
+          this.contact = contact;
+          return (this.error = '');
+        }
+
+        return (this.error = 'Contact not found');
+      })
+      .catch(err => {
+        this.error = 'Error finding contact';
+      });
+  }
+
+  edit() {}
+}
