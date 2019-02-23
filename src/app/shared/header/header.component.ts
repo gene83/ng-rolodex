1648d02c72b53ac4823c405cc6587911eec2b53f
Gene83 <gene83@protonmail.com>
2019-02-22 17:56:48 -1000

    add files for header.component and write out component.ts with isLoggedIn and logout functions

---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { SessionService } from '../../services/session.service';
+
+@Component({
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  user: Object;
+
+  constructor(
+    private router: Router,
+    private auth: AuthService,
+    private session: SessionService
+  ) {
+    this.user = this.session.getSession();
+  }
+
+  isLoggedIn() {
+    return this.session.isLoggedIn;
+  }
+
+  logout() {
+    return this.auth.logout();
+  }
+}
