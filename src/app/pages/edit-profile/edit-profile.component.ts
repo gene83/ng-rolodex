0135b2a8e7546f888d4bf46256b8ab119a07c507
Gene83 <gene83@protonmail.com>
2019-02-23 13:49:10 -1000

    create files for, and write edit-profile.component.ts

---
+++ src/app/pages/edit-profile/edit-profile.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { BackendService } from '../../services/backend.service';
+import { Router } from '@angular/router';
+
+@Component({
+  templateUrl: './edit-profile.component.html',
+  styleUrls: ['./edit-profile.component.scss']
+})
+export class EditProfileComponent {
+  editedProfile: Object;
+  error: string = '';
+
+  constructor(private backend: BackendService, private router: Router) {}
+
+  ngOnInit() {
+    this.backend
+      .getProfile()
+      .then(profile => {
+        this.editedProfile = profile;
+        this.error = '';
+      })
+      .catch(err => {
+        this.error = 'Error finding profile';
+      });
+  }
+
+  edit() {
+    this.backend
+      .editProfile(this.editedProfile)
+      .then(() => {
+        this.router.navigate(['/profile']);
+      })
+      .catch(err => (this.error = 'Error updating Profile'));
+  }
+}
